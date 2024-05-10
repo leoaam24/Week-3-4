6b683844761134edd5de00163b211201e7566391
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2024-05-11 00:29:55 +0800

    created new node server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+const port = process.env.port || 3000;
+const app = express();
+
+
+
+
+
