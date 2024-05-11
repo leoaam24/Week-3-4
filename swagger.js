f251e533a19da99109f5e93a80453f038ff52543
leoaam24 <102619114+leoaam24@users.noreply.github.com>
2024-05-11 22:19:08 +0800

    changes in env, and host

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'Users Api',
         description: 'Users Api'
     },
-    host: 'localhost:3000',
-    schemes: ['http', 'https']
+    host: 'https://week-3-4-edpm.onrender.com',
+    schemes: ['https']
 };
 
 const outputFile = './swagger.json';
